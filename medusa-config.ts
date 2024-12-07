aee54d5732b674f2a12735adc948cd3b364e3125
James Fincher <james@fincher.dev>
2024-12-06 18:05:35 -0800

    adjusting railway

--- medusa-config.ts
+++ medusa-config.ts
@@ -38,14 +38,14 @@ const modules = {
           resolve: "@medusajs/medusa/file-s3",
           id: "s3",
           options: {
-            endpoint: "http://bucket.railway.internal:9000", // Base MinIO server URL
-            file_url: "http://bucket.railway.internal:9000/medusa", // URL with bucket
+            endpoint: "http://bucket.railway.internal:9000", // Internal endpoint for uploads
+            file_url: "https://console-production-d366.up.railway.app/medusa", // Public URL for viewing
             access_key_id: process.env.S3_ACCESS_KEY_ID,
             secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
             bucket: "medusa",
             region: "us-east-1",
             additional_client_config: {
-              forcePathStyle: true, // Required for MinIO as per docs
+              forcePathStyle: true,
             },
           },
         },
